9ff17e4ad8e8fbc1283c3f9762fab9634ff29f85
Maksim Golubev <maksim.golubev@billing.ru>
2017-09-07 18:33:37 +0300

    allow delete tags separately from creating tags

--- app/Remotes2LocalMode.hs
+++ app/Remotes2LocalMode.hs
@@ -5,7 +5,7 @@ module Remotes2LocalMode (run) where
 
 import System.IO (stdout, hFlush)
 import Data.Monoid ((<>))
-import CLIFlags (Flags(Remotes2LocalFlags), tag, no_tag, branch, no_branch, repo, no_repo, stay_orig_tags, dry_run, force)
+import CLIFlags (Flags(Remotes2LocalFlags), tag, no_tag, branch, no_branch, repo, no_repo, tag_del, no_tag_del, dry_run, force)
 import GitCommits
 import Control.Monad.Reader
 import Control.Monad.State
@@ -37,6 +37,7 @@ data Conf = Conf { getFlags :: Flags
                  , getCommits :: GitCommitsMap
                  } deriving (Show)
 type TagSubstitutions = Map.Map RefName (Set.Set RefName)
+type TagDeletions = Set.Set RefName
 type BranchSubstitutions = Set.Set (RepoName, RefName)
 type MetRepoNames = Set.Set RepoName
 type PassedCommits = Set.Set GitHash
@@ -45,11 +46,21 @@ data TraverseState = TraverseState
                        metRepoNames :: MetRepoNames
                      , passedCommits :: PassedCommits
                      , tagSubs :: TagSubstitutions
+                     , tagDels :: TagDeletions
                      , branchSubs :: BranchSubstitutions
                      } deriving (Show)
 
 makeConf = Conf
-initialState = TraverseState {metRepoNames = Set.empty, passedCommits = Set.empty, tagSubs = Map.empty, branchSubs = Set.empty}
+initialState = TraverseState {metRepoNames = Set.empty, passedCommits = Set.empty, tagSubs = Map.empty, tagDels = Set.empty, branchSubs = Set.empty}
+
+hasProposedChanges :: (MonadState TraverseState m) => m Bool
+hasProposedChanges = do
+  s <- get
+  return . any ($ s) $
+    [ not . Map.null . tagSubs
+    , not . Set.null . tagDels
+    , not . Set.null . branchSubs
+    ]
 
 process :: (MonadReader Conf m, MonadState TraverseState m, MonadIO m) => m ()
 process = do
@@ -68,42 +79,45 @@ process = do
       if force'
         then return True
         else do
-          stayOrigTags <- stay_orig_tags . getFlags <$> ask
-          tagSubsts <- tagSubs <$> get
+          tagCreateCount <- Map.foldl (\s v -> s + Set.size v) 0 . tagSubs <$> get
+          tagDeleteCount <- Set.size . tagDels <$> get
           branchSubsCount <- Set.size . branchSubs <$> get
-          let
-            tagCreateCount = Map.foldl (\s v -> s + Set.size v) 0 tagSubsts
-            tagDeleteCount = Map.size tagSubsts
 
-          liftIO $ putStrLn "Proposed changes:"
-          liftIO . putStrLn $ "  " <> show tagCreateCount <> " tag(s) will be created"
-          unless stayOrigTags $ liftIO . putStrLn $ "  " <> show tagDeleteCount <> " tag(s) will be deleted"
-          liftIO . putStrLn $ "  " <> show branchSubsCount <> " branch(es) will be created"
-          (\x -> x == "Y" || x == "y") <$> liftIO (putStr "Are you sure? [Y/N]: " >> hFlush stdout >> getLine)
+          if (tagCreateCount + tagDeleteCount + branchSubsCount) == 0
+            then
+              return True
+            else do
+              liftIO $ putStrLn "Proposed changes:"
+              liftIO . putStrLn $ "  " <> show tagCreateCount <> " tag(s) will be created"
+              liftIO . putStrLn $ "  " <> show tagDeleteCount <> " tag(s) will be deleted"
+              liftIO . putStrLn $ "  " <> show branchSubsCount <> " branch(es) will be created"
+              (\x -> x == "Y" || x == "y") <$> liftIO (putStr "Are you sure? [Y/N]: " >> hFlush stdout >> getLine)
     mustRun :: (MonadReader Conf m, MonadState TraverseState m, MonadIO m) => m Bool
     mustRun = do
-      isProposedChangesAbsent <- (&&) <$> (Map.null . tagSubs <$> get) <*> (Set.null . branchSubs <$> get)
+      hasProposedChanges' <- hasProposedChanges
       dryRun <- dry_run . getFlags <$> ask
-      if isProposedChangesAbsent then return False
-      else askUserConfirmation
+      if not hasProposedChanges'
+        then return False
+        else askUserConfirmation
 
 applyTagSubstitutions :: (MonadReader Conf m, MonadState TraverseState m, MonadIO m) => m ()
-applyTagSubstitutions =
+applyTagSubstitutions = do
   gets tagSubs >>= mapM_ processOneTagSubstitution . fmap (fmap Set.toList) . Map.toList
+  gets tagDels >>= mapM_ processOneTagDeletion . Set.toList
   where
     processOneTagSubstitution :: (MonadReader Conf m, MonadIO m) => (RefName, [RefName]) -> m ()
     processOneTagSubstitution (oldName, newNames) = do
       dryRun <- dry_run . getFlags <$> ask
       mapM_ (\newName -> liftIO . runCom dryRun $ "git tag " <> newName <> " " <> oldName) newNames
-      stay_orig_tags' <- stay_orig_tags . getFlags <$> ask
-      unless stay_orig_tags' (liftIO $ runCom dryRun $ "git tag --delete " <> oldName)
+    processOneTagDeletion :: (MonadReader Conf m, MonadIO m) => RefName -> m ()
+    processOneTagDeletion oldName = do
+      dryRun <- dry_run . getFlags <$> ask
+      liftIO $ runCom dryRun $ "git tag --delete " <> oldName
 
 getTagSubstitutions :: (MonadReader Conf m, MonadState TraverseState m) => m ()
 getTagSubstitutions = do
-  ignoreTags <- elem ".*" . no_tag . getFlags <$> ask
-  unless ignoreTags $ do
-    topCommits <- getTopCommits . getCommits <$> ask
-    mapM_ traverseForTagWithClearRepoNames topCommits
+  topCommits <- getTopCommits . getCommits <$> ask
+  mapM_ traverseForTagWithClearRepoNames topCommits
   where
     traverseForTagWithClearRepoNames commit = do
       st <- get
@@ -139,17 +153,19 @@ traverseForTag commit = do
       conf <- ask
       TraverseState{metRepoNames = metRepoNames'} <- get
       let
-        existingTags = filter (isMatchTag conf) . getTags $ commit
+        existingTags = getTags commit
+        selectedTags4Create = filter (not . isPrefixedWithAnyRepoName) . filter (isMatchTag conf) $ existingTags
+        selectedTags4Deletion = filter (not . isPrefixedWithAnyRepoName) . filter (isMatchTagDel conf) $ existingTags
         isTagAlreadyExists tagName = tagName `elem` existingTags
         selectedRepoNamesList = filter (isMatchRepoName conf) . Set.toList $ metRepoNames'
         getNewTagName repoName originTagName = repoName <> "/" <> originTagName
         allRepoNames = Set.toList . Set.fromList . concatMap (getRemoteRepoNames isSuitableRepoName) . toCommits . getCommits $ conf
         isPrefixedWithAnyRepoName tagName = any (`T.isPrefixOf` (tagName <> "/")) allRepoNames
       mapM_ addTagSubstitution
-        [ (e, getNewTagName r e) | r <- selectedRepoNamesList, e <- existingTags
-                                 , not ( isTagAlreadyExists (getNewTagName r e) || isPrefixedWithAnyRepoName e )]
+        [ (e, getNewTagName r e) | r <- selectedRepoNamesList, e <- selectedTags4Create
+                                 , not (isTagAlreadyExists (getNewTagName r e))]
       unless (Set.null metRepoNames') $
-        mapM_ addTagForDeletion . filter (not . isPrefixedWithAnyRepoName) $ existingTags
+        mapM_ addTagForDeletion selectedTags4Deletion
       where
         addTagSubstitution (origName, newName) = do
           st@TraverseState{tagSubs = tagSubs'} <- get
@@ -159,8 +175,8 @@ traverseForTag commit = do
             combine Nothing = Just $ Set.singleton newName
 
         addTagForDeletion origName = do
-          st@TraverseState{tagSubs = tagSubs'} <- get
-          put st{tagSubs = Map.alter combine origName tagSubs'}
+          st@TraverseState{tagDels = tagDels'} <- get
+          put st{tagDels = Set.union tagDels' $ Set.singleton origName}
           where
             combine (Just x) = Just x
             combine Nothing = Just Set.empty
@@ -201,36 +217,47 @@ applyBranchSubstitutions =
       where
         newBranchName' = newBranchName repoName refName
 
+isMatch :: [String] -> [String] -> T.Text -> Bool
+isMatch positive negative name =
+  let
+    checkRegex regex = T.unpack name =~ regex :: Bool
+    isPositive = null positive || any checkRegex positive
+    isNegative = any checkRegex negative
+  in isPositive && not isNegative
+
 isMatchRepoName :: Conf -> RepoName -> Bool
 isMatchRepoName conf repoName =
   let
-    flags = getFlags conf
-    repo' = repo flags
-    no_repo' = no_repo flags
-    checkRegex regex = T.unpack repoName =~ regex :: Bool
-    isSelected = null repo' || any checkRegex repo'
-    isIgnored = any checkRegex no_repo'
+    positive = repo . getFlags $ conf
+    negative = no_repo . getFlags $ conf
     isOriginRepo = not . isSuitableRepoName $ repoName
-  in not isOriginRepo && isSelected && not isIgnored
+  in not isOriginRepo && (isMatch positive negative repoName)
 
 isMatchTag :: Conf -> RefName -> Bool
 isMatchTag conf tagName =
   let
-    flags = getFlags conf
-    tag' = tag flags
-    no_tag' = no_tag flags
-    checkRegex regex = T.unpack tagName =~ regex :: Bool
-    isSelected = null tag' || any checkRegex tag'
-    isIgnored = any checkRegex no_tag'
-  in isSelected && not isIgnored
+    positive = tag . getFlags $ conf
+    negative = no_tag . getFlags $ conf
+  in
+    isMatch positive negative tagName
+
+isMatchTagDel :: Conf -> RefName -> Bool
+isMatchTagDel conf tagName =
+  let
+    positive1 = tag_del . getFlags $ conf
+    negative1 = no_tag_del . getFlags $ conf
+    assigned = not (null positive1 && null negative1)
+    positive2 = tag . getFlags $ conf
+    negative2 = no_tag . getFlags $ conf
+    positive = if assigned then positive1 else positive2
+    negative = if assigned then negative1 else negative2
+  in
+    isMatch positive negative tagName
 
 isMatchBranch :: Conf -> RefName -> Bool
 isMatchBranch conf branchName =
   let
-    flags = getFlags conf
-    branch' = branch flags
-    no_branch' = no_branch flags
-    checkRegex regex = T.unpack branchName =~ regex :: Bool
-    isSelected = null branch' || any checkRegex branch'
-    isIgnored = any checkRegex no_branch'
-  in isSelected && not isIgnored
+    positive = branch . getFlags $ conf
+    negative = no_branch . getFlags $ conf
+  in
+    isMatch positive negative branchName
